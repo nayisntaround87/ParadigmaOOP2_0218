37681d6bed9461b98b7aef5e01861cba537c796b
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-28 15:50:22 +0700

    set lampu rumah

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -18,5 +18,12 @@ public :
 
 int main()
 {
-    remoteLampu
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    
 }
